ce377da5c58a24287e8377ab314b839c6e1df901
ci <ci@banuba.com>
2024-07-29 18:54:22 +0300

    1.14.1-67-g77a5e386d

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.14.1-61-gc1c7cef54"
+let bnbPackageVersion: Version = "1.14.1-67-g77a5e386d"
 
 let package = Package(
     name: "BNBAcneEyebagsRemoval",
@@ -41,8 +41,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BNBAcneEyebagsRemoval",
-            url: "https://d2cm7wtcqqw29.cloudfront.net/1.14.1-61-gc1c7cef54/BNBAcneEyebagsRemoval.zip",
-            checksum: "acf3bdca1a725dccb2ccbe869e231bcd7b96cb068f3e67b0d75a7ecb7fdea8d9"
+            url: "https://d2cm7wtcqqw29.cloudfront.net/1.14.1-67-g77a5e386d/BNBAcneEyebagsRemoval.zip",
+            checksum: "98e2215abc9f7ed6c4a6b3cf33c537e44aefd6585971da5a33e27c9dc23ed76b"
         ),
         .target(
             name: "BNBAcneEyebagsRemoval_BNBSdkCore",
